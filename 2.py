a39e57523e2bd5eae8f2d6de2529c227857ec146
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2015-02-12 00:54:22 +0200

    Added comments to 4.4 and comment with code mistake

--- 2.py
+++ 2.py
@@ -3,8 +3,10 @@ import sys
 x = int(sys.argv[1])
 y = int(sys.argv[2])
 gCounter = 0
-increment = 1
+SIncrement = 0
+fIncrement = 0
 
+# add nulls, when num doesn`t composed of six digits
 def makeStringWithNull(num):
 	if (num < 9):
 		num = "00000"+str(num)
@@ -20,22 +22,38 @@ def makeStringWithNull(num):
 		num = str(num)
 	return num
 
+# get sum of all digits in num
 def getSum(num):
 	sum = 0
 	for i in str(num):
 		sum = sum + int(i)
 	return sum
 
+# create cycle, whick iterate max-min times
 for i in range( max(x,y) - min(x,y) ):
-	fHalf = int(makeStringWithNull(min(x,y))[0:3])
-	sHalf = int(makeStringWithNull(min(x,y))[3:])+increment
+	# fHalf and sHalf - first and second halfs of min number
+	fHalf = int(makeStringWithNull(min(x,y))[0:3]) + fIncrement
+	sHalf = int(makeStringWithNull(min(x,y))[3:]) + SIncrement
+	print "Second Half"
+	print sHalf
+	print "SIncrement"
+	print SIncrement
+	# if we found that lucky num SIncrement counter
 	if(getSum(fHalf) == getSum(sHalf)):
 		gCounter = gCounter + 1
+	# gradually increase second half of number for 1 and if it is over 999 SIncrement firstHalf by 1
+	# for example if we have 223999, next num in cycle will be 224000
+
+
+	# need to make sHalf null after this IF!!!!!!!!!!
 	
+
+
+
 	if (sHalf == 999):
-		fHalf = fHalf + 1
-	print sHalf
-	print fHalf
-	increment = increment + 1
+		fIncrement = fIncrement + 1
+		SIncrement = 0
+	# increse second half by 1
+	SIncrement = SIncrement + 1
 
 print gCounter+1
